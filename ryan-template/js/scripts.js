e9fc3c2d406289766241e9df7427109ba09f96ea
Talish <talishesu@code.edu.az>
2022-01-08 10:22:57 +0400

    First Commit

---
+++ ryan-template/js/scripts.js
@@ -0,0 +1,601 @@
+/*
+*   Author: beshleyua
+*   Author URL: http://themeforest.net/user/beshleyua
+*/
+
+
+/*
+	Preloader
+*/
+
+$(window).on("load", function() {
+	var preload = $('.preloader');
+	preload.find('.spinner').fadeOut(function(){
+		preload.fadeOut();
+	});
+});
+
+$(function () {
+	'use strict';
+	
+	
+	/*
+		Vars
+	*/
+	
+	var width = $(window).width();
+	var height = $(window).height();
+
+
+	/*
+		Typed
+	*/
+
+	$('.subtitle.subtitle-typed').each(function(){
+		var subtitleContainer = $(this);
+
+		subtitleContainer.typed({
+			stringsElement: subtitleContainer.find('.typing-title'),
+			backDelay: 3500, /* Delay in text change */
+			typeSpeed: 0, /* Typing speed */
+			loop: true
+		});
+	});
+
+
+	/*
+		Sidebar Show/Hide
+	*/
+
+	$('header, .profile').on('click', '.menu-btn', function(){
+		$('.s_overlay').fadeIn();
+		$('.content-sidebar').addClass('active');
+		$('body,html').addClass('sidebar-open');
+		return false;
+	});
+	$('.content-sidebar, .container').on('click', '.close, .s_overlay', function(){
+		$('.s_overlay').fadeOut();
+		$('.content-sidebar').removeClass('active');
+		$('body,html').removeClass('sidebar-open');
+	});
+
+
+	/*
+		Popup Menu Navigation
+	*/
+	
+	$('.main-menu li.page_item_has_children').each(function(){
+		$(this).find('> a').after('<span class="children_toggle"></span>');
+	});
+	$('.main-menu').on('click', '.children_toggle', function(){
+		var main_menu_item = $(this).closest('.page_item_has_children');
+		if(main_menu_item.hasClass('open')) {
+			main_menu_item.removeClass('open');
+			main_menu_item.find('> ul').slideUp(250);
+		} else {
+			main_menu_item.addClass('open');
+			main_menu_item.find('> ul').slideDown(250);
+		}
+	});
+
+
+	/*
+		Default Menu
+	*/
+
+	$('.lnk-view-menu').on('click', function(){
+		var btn_text1 = $(this).find('.text').text();
+		var btn_text2 = $(this).find('.text').data('text-open');
+		if($('.profile').hasClass('default-menu-open')){
+			$('.profile').removeClass('default-menu-open');
+			$(this).find('.text').data('text-open', btn_text1);
+			$(this).find('.text').text(btn_text2);
+		} else {
+			$('.profile').addClass('default-menu-open');
+			$(this).find('.text').data('text-open', btn_text1);
+			$(this).find('.text').text(btn_text2);
+		}
+
+		return false;
+	});
+	
+	
+	/*
+		Header Menu Desktop
+	*/
+	
+	var container = $('.container');
+	var card_items = $('.card-inner');
+	var animation_in = container.data('animation-in');
+	var animation_out = container.data('animation-out');
+	
+	$('.top-menu').on('click', 'a', function(){
+
+		/* vars */
+		var width = $(window).width();
+		var id = $(this).attr('href');
+		var h = parseFloat($(id).offset().top);
+		var card_item = $(id);
+		var menu_items = $('.top-menu li');
+		var menu_item = $(this).closest('li');
+		var d_lnk = $('.lnks .lnk.discover');
+
+		if((width >= 1024)) {
+			
+			/* if desktop */
+			if(!menu_item.hasClass('active') & (width > 1023) & $('#home-card').length) {
+
+				/* close card items */
+				menu_items.removeClass('active');
+				container.find(card_items).removeClass('animated '+animation_in);
+
+				if($(container).hasClass('opened')) {
+					container.find(card_items).addClass('animated '+animation_out);
+				}
+
+				/* open card item */
+				menu_item.addClass('active');
+				container.addClass('opened');
+				container.find(card_item).removeClass('animated '+animation_out);
+				container.find(card_item).addClass('animated '+animation_in);
+				
+				$(card_items).addClass('hidden');
+				
+				$(card_item).removeClass('hidden');
+				$(card_item).addClass('active');
+			}
+		}
+		/* if mobile */
+		if((width < 1024) & $('#home-card').length) {
+
+			/* scroll to section */
+			$('body,html').animate({
+				scrollTop: h - 76
+			}, 800);
+		}
+		return false;
+	});
+
+	$(window).on('resize', function(){
+		var width = $(window).width();
+		var height = $(window).height();
+
+		if((width < 1024)) {
+			$('.card-inner').removeClass('hidden');
+			$('.card-inner').removeClass('fadeOutLeft');
+			$('.card-inner').removeClass('rotateOutUpLeft');
+			$('.card-inner').removeClass('rollOut');
+			$('.card-inner').removeClass('jackOutTheBox');
+			$('.card-inner').removeClass('fadeOut');
+			$('.card-inner').removeClass('fadeOutUp');
+			$('.card-inner').removeClass('animated');
+
+			$(window).on('scroll', function(){
+				var scrollPos = $(window).scrollTop();
+				$('.top-menu ul li a').each(function () {
+					var currLink = $(this);
+					var refElement = $(currLink.attr("href"));
+					if (refElement.offset().top - 76 <= scrollPos) {
+						$('.top-menu ul li').removeClass("active");
+						currLink.closest('li').addClass("active");
+					}
+				});
+			});
+
+			$('.card-inner .card-wrap').slimScroll({destroy: true});
+			$('.card-inner .card-wrap').attr('style', '');
+		}
+		else {
+			$($('.top-menu li.active a').attr('href')).addClass('active');
+			if((!$('.page').hasClass('new-skin')) && (width > 1024)) {
+				$('.card-inner .card-wrap').slimScroll({
+					height: '570px'
+				});
+			}
+		}
+	});
+	
+	
+	/*
+		Smoothscroll
+	*/
+	
+	if((width < 1024) & $('#home-card').length) { 
+		$(window).on('scroll', function(){
+			var scrollPos = $(window).scrollTop();
+			$('.top-menu ul li a').each(function () {
+				var currLink = $(this);
+				var refElement = $(currLink.attr("href"));
+				if (refElement.offset().top - 76 <= scrollPos) {
+					$('.top-menu ul li').removeClass("active");
+					currLink.closest('li').addClass("active");
+				}
+			});
+		});
+	}
+	
+	
+	/*
+		slimScroll
+	*/
+	
+	if((!$('.page').hasClass('new-skin')) && (width > 1024)) {
+		$('.card-inner .card-wrap').slimScroll({
+			height: '570px'
+		});
+	}
+	
+	
+	/*
+		Hire Button
+	*/
+	
+	$('.lnks').on('click', '.lnk.discover', function(){
+		$('.top-menu a[href="#contacts-card"]').trigger('click');
+	});
+	
+	
+	/*
+		Initialize Portfolio
+	*/
+	var $container = $('.grid-items');
+	$container.imagesLoaded(function() {
+		$container.isotope({
+			percentPosition: true,
+			itemSelector: '.grid-item'
+		});
+	});
+
+
+	/*
+		Filter items on button click
+	*/
+	$('.filter-button-group').on( 'click', '.f_btn', function() {
+		var filterValue = $(this).find('input').val();
+		$container.isotope({ filter: '.'+filterValue });
+		$('.filter-button-group .f_btn').removeClass('active');
+		$(this).addClass('active');
+	});
+
+	
+	/*
+		Gallery popup
+	*/
+	if(/\.(?:jpg|jpeg|gif|png)$/i.test($('.gallery-item:first a').attr('href'))){
+		$('.gallery-item a').magnificPopup({
+			gallery: {
+				enabled: true
+			},
+			type: 'image',
+			closeBtnInside: false,
+			mainClass: 'mfp-fade'
+		});
+	}
+
+
+	/*
+		Media popup
+	*/
+	$('.has-popup-media').magnificPopup({
+		type: 'inline',
+		overflowY: 'auto',
+		closeBtnInside: true,
+		mainClass: 'mfp-fade popup-box-inline'
+	});
+
+
+	/*
+		Image popup
+	*/
+	$('.has-popup-image').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-fade',
+		image: {
+			verticalFit: true
+		}
+	});
+
+	
+	/*
+		Video popup
+	*/
+	$('.has-popup-video').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		iframe: {
+			patterns: {
+				youtube_short: {
+				  index: 'youtu.be/',
+				  id: 'youtu.be/',
+				  src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+				}
+			}
+		},
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false,
+		mainClass: 'mfp-fade',
+		callbacks: {
+			markupParse: function(template, values, item) {
+				template.find('iframe').attr('allow', 'autoplay');
+			}
+		}
+	});
+
+	
+	/*
+		Music popup
+	*/
+	$('.has-popup-music').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false,
+		mainClass: 'mfp-fade'
+	});
+
+
+	/*
+		Gallery popup
+	*/
+	$('.has-popup-gallery').on('click', function() {
+		var gallery = $(this).attr('href');
+	
+		$(gallery).magnificPopup({
+			delegate: 'a',
+			type:'image',
+			closeOnContentClick: false,
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			fixedContentPos: false,
+			gallery: {
+				enabled: true
+			}
+		}).magnificPopup('open');
+
+		return false;
+	});
+	
+	
+	/*
+		Validate Contact Form
+	*/
+	
+	$("#cform").validate({
+		ignore: ".ignore",
+		rules: {
+			name: {
+				required: true
+			},
+			message: {
+				required: true
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			hiddenRecaptcha: {
+				required: function () {
+					if (grecaptcha.getResponse() == '') {
+						return true;
+					} else {
+						return false;
+					}
+				}
+			}
+		},
+		success: "valid",
+		submitHandler: function() {
+			$.ajax({
+				url: 'mailer/feedback.php',
+				type: 'post',
+				dataType: 'json',
+				data: 'name='+ $("#cform").find('input[name="name"]').val() + '&email='+ $("#cform").find('input[name="email"]').val() + '&message=' + $("#cform").find('textarea[name="message"]').val(),
+				beforeSend: function() {
+				
+				},
+				complete: function() {
+				
+				},
+				success: function(data) {
+					$('#cform').fadeOut();
+					$('.alert-success').delay(1000).fadeIn();
+				}
+			});
+		}
+	});
+	
+	
+	/*
+		Validate Commect Form
+	*/
+	
+	$("#comment_form").validate({
+		rules: {
+			name: {
+				required: true
+			},
+			message: {
+				required: true
+			}
+		},
+		success: "valid",
+		submitHandler: function() {
+		}
+	});
+	
+	
+	/*
+		Google Maps
+	*/
+	
+	if($('#map').length) {
+		initMap();
+	}
+
+
+	/*
+		Tesimonials Carousel
+	*/
+	var revs_slider = $(".revs-carousel.default-revs .owl-carousel");
+
+	revs_slider.owlCarousel({
+		margin: 0,
+		items: 1,
+		autoplay: false,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		loop: true,
+		rewind: false,
+		nav: false,
+		dots: true
+	});
+
+	var rtl_revs_slider = $(".revs-carousel.rtl-revs .owl-carousel");
+
+	rtl_revs_slider.owlCarousel({
+		margin: 0,
+		items: 1,
+		rtl: true,
+		autoplay: false,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		loop: true,
+		rewind: false,
+		nav: false,
+		dots: true
+	});
+
+
+	/*
+		New JS
+	*/
+
+	$(window).on('resize', function(){
+		/*
+			Dotted Skills Line On Resize Window
+		*/
+
+		var skills_dotted = $('.skills-list.dotted .progress');
+		var skills_dotted_w = skills_dotted.width();
+		if(skills_dotted.length){
+			skills_dotted.find('.percentage .da').css({'width':skills_dotted_w+1});
+		}
+
+		/*
+			Testimonials Carousel On Resize Window
+		*/
+
+		var revs_slider = $(".revs-carousel .owl-carousel");
+		revs_slider.find('.revs-item').css({'max-width':revs_slider.width()});
+	});
+
+	/*
+		Dotted Skills Line
+	*/
+
+	function skills(){
+		var skills_dotted = $('.skills-list.dotted .progress');
+		var skills_dotted_w = skills_dotted.width();
+		if(skills_dotted.length){
+			skills_dotted.append('<span class="dg"><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span></span>');
+			skills_dotted.find('.percentage').append('<span class="da"><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span></span>');
+			skills_dotted.find('.percentage .da').css({'width':skills_dotted_w});
+		}
+	}
+	setTimeout(skills, 1000);
+
+	/*
+		Circle Skills Line
+	*/
+
+	var skills_circles = $('.skills-list.circles .progress');
+	if(skills_circles.length){
+		skills_circles.append('<div class="slice"><div class="bar"></div><div class="fill"></div></div>');
+	}
+
+	/*
+		Wrap First Title Word
+	*/
+
+	$('.content .title').each(function(index) {
+	    var title = $(this).text().split(' ');
+	    if(title.length>1){
+		    var firstWord = title[0];
+		    var replaceWord = '<span class="first-word">' + firstWord + '</span>';
+		    var newString = $(this).html().replace(firstWord, replaceWord);
+		    $(this).html(newString);
+		} else {
+			$(this).html('<div class="first-letter">'+ $(this).html() + '</div>');
+		}
+	});
+
+});
+
+
+/*
+	Google Map Options
+*/
+
+function initMap() {
+	var myLatlng = new google.maps.LatLng(40.773328,-73.960088); // <- Your latitude and longitude
+	var styles = [
+	{
+		"featureType": "water",
+		"stylers": [{
+			"color": "#d8dee9"
+		},
+		{
+			"visibility": "on"
+		}]
+	},
+	{
+		"featureType": "landscape",
+		"stylers": [{
+			"color": "#eeeeee"
+		}]
+	}]
+
+	var mapOptions = {
+		zoom: 14,
+		center: myLatlng,
+		mapTypeControl: false,
+		disableDefaultUI: true,
+		zoomControl: true,
+		scrollwheel: false,
+		styles: styles
+	}
+	
+	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		title: 'We are here!'
+	});
+}
+
+( function( $ ) {
+	'use strict';
+	
+	// load html
+	$.get("https://bslthemes.site/html/ryan/bar/bar.html", function (data) {
+        $('body').append(data);
+ 		console.log(data);
+    });
+	
+} )( jQuery );
+
+$(function() {
+$('body').append('<div class="fix-bts" style=" position: fixed; top: 50vh; right: -74px; z-index: 1111; transition: all 0.2s ease 0s;"> <a target="blank" title="Check our portfolio on Themeforest" href="https://1.envato.market/JERja" class="fix-btn brand" style=" position: relative; margin-bottom: 5px; padding: 0 10px; display: block; height: 38px; line-height: 38px; background: #fff; text-transform: uppercase; font-size: 11px; box-shadow: 0 5px 30px rgba(0, 0, 0, 0.1); font-weight: 700; text-decoration: none; color: #000; "><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="415.441px" height="415.441px" viewBox="0 0 415.441 415.441" xml:space="preserve" style=" width: 18px; height: 38px; display: inline-block; vertical-align: middle; margin-right: 10px; "> <g> <g> <path fill="#7cb342" d="M324.63,22.533C135.173,226.428,80.309,371.638,80.309,371.638c41.149,47.743,111.28,43.72,111.28,43.72 c73.921,2.31,119.192-43.522,119.192-43.522c91.861-92.516,80.549-355.302,80.549-355.302 C372.769-23.891,324.63,22.533,324.63,22.533z"></path> <path fill="#7cb342" d="M32.369,181.983c0,0-28.983,57.964,18.859,155.495L178.367,58.01C176.916,58.538,63.691,98.037,32.369,181.983z"></path> </g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </svg>Beshleyua</a> <a target="blank" title="Buy HTML template on Themeforest" href="https://1.envato.market/40qOL" class="fix-btn bh" style=" position: relative; margin-bottom: 5px; padding: 0 10px; display: none; height: 38px; line-height: 38px; background: #fff; text-transform: uppercase; font-size: 11px; box-shadow: 0 5px 30px rgb(0 0 0 / 10%); font-weight: 700; text-decoration: none; color: #000; "><svg id="Capa_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg" style=" width: 18px; height: 38px; display: inline-block; vertical-align: middle; margin-right: 10px; "><path d="m504.399 185.065c-6.761-8.482-16.904-13.348-27.83-13.348h-98.604l-53.469-122.433c-3.315-7.591-12.157-11.06-19.749-7.743-7.592 3.315-11.059 12.158-7.743 19.75l48.225 110.427h-178.458l48.225-110.427c3.315-7.592-.151-16.434-7.743-19.75-7.591-3.317-16.434.15-19.749 7.743l-53.469 122.434h-98.604c-10.926 0-21.069 4.865-27.83 13.348-6.637 8.328-9.086 19.034-6.719 29.376l52.657 230c3.677 16.06 17.884 27.276 34.549 27.276h335.824c16.665 0 30.872-11.216 34.549-27.276l52.657-230.001c2.367-10.342-.082-21.048-6.719-29.376zm-80.487 256.652h-335.824c-2.547 0-4.778-1.67-5.305-3.972l-52.657-229.998c-.413-1.805.28-3.163.936-3.984.608-.764 1.985-2.045 4.369-2.045h85.503l-3.929 8.997c-3.315 7.592.151 16.434 7.743 19.75 1.954.854 3.99 1.258 5.995 1.258 5.782 0 11.292-3.363 13.754-9l9.173-21.003h204.662l9.173 21.003c2.462 5.638 7.972 9 13.754 9 2.004 0 4.041-.404 5.995-1.258 7.592-3.315 11.059-12.158 7.743-19.75l-3.929-8.997h85.503c2.384 0 3.761 1.281 4.369 2.045.655.822 1.349 2.18.936 3.983l-52.657 230c-.528 2.301-2.76 3.971-5.307 3.971z"></path><path d="m166 266.717c-8.284 0-15 6.716-15 15v110c0 8.284 6.716 15 15 15s15-6.716 15-15v-110c0-8.284-6.715-15-15-15z"></path><path d="m256 266.717c-8.284 0-15 6.716-15 15v110c0 8.284 6.716 15 15 15s15-6.716 15-15v-110c0-8.284-6.716-15-15-15z"></path><path d="m346 266.717c-8.284 0-15 6.716-15 15v110c0 8.284 6.716 15 15 15s15-6.716 15-15v-110c-.001-8.284-6.716-15-15-15z"></path></svg>Buy HTML</a> <a target="blank" title="Buy WordPress theme on Themeforest" href="https://1.envato.market/e241D" class="fix-btn wp" style="display: none!important; position: relative; margin-bottom: 5px; padding: 0 10px; display: block; height: 38px; line-height: 38px; background: #fff; text-transform: uppercase; font-size: 11px; box-shadow: 0 5px 30px rgb(0 0 0 / 10%); font-weight: 700; text-decoration: none; color: #000; "><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 300 300" xml:space="preserve" style=" width: 18px; height: 38px; display: inline-block; vertical-align: middle; margin-right: 10px; "> <g id="XMLID_21_"> <path id="XMLID_4_" fill="#297197" d="M0.005,149.995c0,59.367,34.502,110.683,84.543,134.992L12.994,88.953 C4.674,107.607,0.005,128.25,0.005,149.995z M251.259,142.429c0-18.544-6.661-31.377-12.367-41.366 c-7.602-12.362-14.729-22.817-14.729-35.18c0-13.788,10.449-26.621,25.184-26.621c0.668,0,1.301,0.081,1.943,0.122 C224.608,14.932,189.049,0,150,0C97.597,0,51.497,26.89,24.674,67.603c3.525,0.116,6.843,0.188,9.655,0.188 c15.685,0,39.98-1.907,39.98-1.907c8.083-0.476,9.033,11.406,0.956,12.362c0,0-8.129,0.951-17.163,1.422l54.619,162.478 l32.827-98.447L122.18,79.667c-8.083-0.471-15.73-1.422-15.73-1.422c-8.083-0.48-7.138-12.838,0.95-12.362 c0,0,24.766,1.907,39.505,1.907c15.686,0,39.98-1.907,39.98-1.907c8.083-0.476,9.039,11.406,0.956,12.362 c0,0-8.138,0.951-17.162,1.422l54.203,161.238l14.962-49.984C247.45,171.427,251.259,155.266,251.259,142.429z M152.636,163.116 L107.628,293.89C121.067,297.841,135.28,300,150,300c17.471,0,34.224-3.015,49.813-8.503c-0.395-0.643-0.769-1.324-1.072-2.068 L152.636,163.116z M281.624,78.038c0.647,4.774,1.012,9.898,1.012,15.422c0,15.215-2.854,32.326-11.412,53.723l-45.812,132.459 c44.598-26,74.583-74.311,74.583-129.646C299.995,123.915,293.328,99.398,281.624,78.038z"></path> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </svg>Buy Theme</a></div>');
+$('.fix-bts').on('mouseenter', function() {
+$('.fix-bts').css({'right':'0'});
+});
+$('.fix-bts').on('mouseleave', function() {
+$('.fix-bts').css({'right':'-74px'});
+});
+if (window.location.href.indexOf("envato-elements") > -1) {
+	$('.fix-bts').hide();
+}
+});
